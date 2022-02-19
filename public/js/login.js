5a35acedd77807806895d05f9cb1cb99c4e6c03a
Gourangi Taware <gourangi0309@gmail.com>
2022-02-19 15:52:42 +0530

    Login Authentication

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("#loginSubmit").click(function (event) {
+        event.preventDefault();
+        var loginemail=$("#login-email").val();
+        var loginpassword=$("#login-password").val();
+        console.log(loginemail);
+        console.log(loginpassword);
+        console.log("In login js");
+        var formData = {
+           loginemail:loginemail,
+           loginpassword:loginpassword
+           };
+       
+           // DO POST
+           $.ajax({
+             type: "POST",
+             contentType: "application/json",
+             url: "register/login",
+             data: JSON.stringify(formData),
+             dataType: "json",
+             success: function (answers) {
+                console.log("In Login Success");
+                location.href="/";
+                console.log(answers);
+                //removeClass(self.element, 'cd-signin-modal--is-visible');
+             },
+             error: function (e) {
+            //    alert("Error!");
+               console.log("ERROR: ", e);
+             },
+           });
+    
+      });
+});
--- routes/register.js
+++ routes/register.js
@@ -14,7 +14,50 @@ router.post('/register', function(req, res, next) {
   return res.send(req.body);
 });
 
+router.post('/login', function(req, res, next) {
+  // Get Form Values
+  console.log("Logining");
+  console.log(req.body);
+  validateEmailAccessibility(req.body.loginemail,req.body.loginpassword).then(function(msg) {
+    if (msg=="Login Successful") {
+      console.log("Login Successful");
+    }
+    else if(msg=="Wrong Password")
+    {
+      console.log("Wrong Password,Try again!")
+    }
+    else {
+      console.log("Email does not exists");
+    }
+  });
+  // req.flash('success', 'User Registered Successfully!Login to continue!');
+  return res.send(req.body);
+});
 
+function validateEmailAccessibility(loginemail,loginpassword){
+
+  return User.findOne({email: loginemail}).then(function(result){
+      
+      console.log(result);
+      
+      if(result)
+      {
+        if(result.password==loginpassword)
+        {
+          return "Login Successful";
+        }
+        else
+        {
+          return "Wrong Password";
+        }
+      }
+      else
+      {
+          return "Email doesn't exist";
+      }
+      
+  });
+}
 
 
 exports.routes = router;
