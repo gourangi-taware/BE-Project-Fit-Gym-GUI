e933d0b846ece97bf4423f9c44c1d46fa4832732
Pratik Dhende <pratikdhende2000@gmail.com>
2022-03-19 12:14:22 +0530

    Added start functionality

--- public/js/model.js
+++ public/js/model.js
@@ -2,6 +2,13 @@ console.log("BicepCurl model connected.");
 const videoElement = document.getElementById('videoModel');
 const canvasElement = document.getElementById('canvasModel');
 const counterElement = document.getElementById('counter');
+const startElement = document.getElementsByClassName('buttonPlay')[0];
+
+startElement.onclick = function fun()
+{
+    startExercise = !startExercise;
+    console.log("Toggled to ", startExercise);
+} 
 
 canvasElement.style.width='100%';
 canvasElement.style.height='100%';
@@ -14,6 +21,7 @@ const repMeterHeight = 100;
 let bicepAngle = 180;
 let count = 0;
 let eccentric = false;
+let startExercise = false;
 
 function onResults(results) {
     canvasCtx.save();
@@ -42,8 +50,11 @@ function onResults(results) {
     drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#00FF00', lineWidth: 4});
     drawLandmarks(canvasCtx, results.poseLandmarks, {color: '#FF0000', lineWidth: 2});
 
-    bicepCheck(results.poseLandmarks);
-    // lungesCheck(results.poseLandmarks);
+    if (startExercise){
+        bicepCheck(results.poseLandmarks);
+    }
+
+    counterElement.innerHTML = count;
 
     canvasCtx.rect(25, 25, 100, 100);
     canvasCtx.stroke();
@@ -87,20 +98,18 @@ function bicepCheck(poseLandmarks){
     shoulder = poseLandmarks[POSE_LANDMARKS.RIGHT_SHOULDER];
     elbow = poseLandmarks[POSE_LANDMARKS.RIGHT_ELBOW];
     wrist = poseLandmarks[POSE_LANDMARKS.RIGHT_WRIST];
-    
+
     if (poseLandmarks[POSE_LANDMARKS.RIGHT_WRIST].visibility > 0.75 && poseLandmarks[POSE_LANDMARKS.RIGHT_ELBOW].visibility > 0.75 && poseLandmarks[POSE_LANDMARKS.RIGHT_SHOULDER].visibility > 0.75){
         console.log("TRUE");
         bicepAngle = rad_to_deg(find_angle_rad(shoulder, elbow, wrist));
-        // bicepAngle = getBicepAngle(poseLandmarks[POSE_LANDMARKS.RIGHT_WRIST], poseLandmarks[POSE_LANDMARKS.RIGHT_ELBOW], poseLandmarks[POSE_LANDMARKS.RIGHT_SHOULDER]);
 
         canvasCtx.fillStyle = "#00FF00";
         canvasCtx.fillRect(25, 25, 100, toRepMeter(bicepAngle, 100, 45, 150));
         
         if (bicepAngle < 45 && !eccentric){
-            // console.log("REP DONE");
+            console.log("REP DONE");
             count += 1;
             eccentric = true;
-            counterElement.innerHTML = count;
         }
         else if (bicepAngle > 150){
             eccentric = false;
@@ -124,6 +133,7 @@ function find_angle_rad(L,M,R) {
     var AC = Math.sqrt(Math.pow(R.x-L.x,2)+ Math.pow(R.y-L.y,2));
     return Math.acos((BC*BC+AB*AB-AC*AC)/(2*BC*AB));
 }
+
 function rad_to_deg(A)
 {
     let X = A * 180/Math.PI;
@@ -157,59 +167,3 @@ const camera = new Camera(videoElement, {
     height: 720
 });
 camera.start();
-
-// var video;
-// var poseNet;
-// var pose;
-// var skeleton;
-
-// function setup() {
-//   createCanvas(640, 480);
-//   video = createCapture(VIDEO);
-//   video.hide();
-//   poseNet = ml5.poseNet(video, modelLoaded);
-//   poseNet.on('pose', gotPoses);
-// }
-
-// function gotPoses(poses) {
-//   //console.log(poses); 
-//   if (poses.length > 0) {
-//     pose = poses[0].pose;
-//     skeleton = poses[0].skeleton;
-//   }
-// }
-
-
-// function modelLoaded() {
-//   console.log('poseNet ready');
-// }
-
-// function draw() {
-//   image(video, 0, 0);
-
-//   if (pose) {
-//     let eyeR = pose.rightEye;
-//     let eyeL = pose.leftEye;
-//     let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
-//     fill(255, 0, 0);
-//     ellipse(pose.nose.x, pose.nose.y, d);
-//     fill(0, 0, 255);
-//     ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
-//     ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
-    
-//     for (let i = 0; i < pose.keypoints.length; i++) {
-//       let x = pose.keypoints[i].position.x;
-//       let y = pose.keypoints[i].position.y;
-//       fill(0,255,0);
-//       ellipse(x,y,16,16);
-//     }
-    
-//     for (let i = 0; i < skeleton.length; i++) {
-//       let a = skeleton[i][0];
-//       let b = skeleton[i][1];
-//       strokeWeight(2);
-//       stroke(255);
-//       line(a.position.x, a.position.y,b.position.x,b.position.y);      
-//     }
-//   }
-// }
